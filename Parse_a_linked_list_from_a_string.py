1d59627d26189edbb4a253fc137cb4e95af8708f
Olena Yakovenko <olena110906@gmail.com>
2025-03-26 11:28:15 +0200

    Convert a linked list to a string/2

--- Parse_a_linked_list_from_a_string.py
+++ Parse_a_linked_list_from_a_string.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -14,37 +13,13 @@ def stringify(node):
 
 def linked_list_from_string(s):
     components = s.split(' -> ')[:-1][::-1]
-    head = Node(components[0])
+    if len(components) < 1:
+        return None
+    head = Node(int(components[0]))
     for el in components[1:]:
-        print(el)
-        head = Node(el, head)
+        head = Node(int(el), head)
 
     return head
 
 res = (linked_list_from_string("1 -> 2 -> 3 -> None"))
-=======
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-def stringify(node):
-    res = ''
-    current = node
-    while current:
-        res += str(current.data) + ' -> '
-        current = current.next
-    return res + "None"
-
-def linked_list_from_string(s):
-    components = s.split(' -> ')[:-1][::-1]
-    head = Node(components[0])
-    for el in components[1:]:
-        print(el)
-        head = Node(el, head)
-
-    return head
-
-res = (linked_list_from_string("1 -> 2 -> 3 -> None"))
->>>>>>> 3490f9948536260b3176b5889b9749e25465dbe7
 print(stringify(res))
